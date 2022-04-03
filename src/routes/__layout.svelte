bc2de660470522c29f5fa181570404c1098e5d9b
NNB <nnbnh@protonmail.com>
2022-04-03 14:51:05 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/routes/__layout.svelte 	modified:   svelte.config.js

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	export const prerender = true;
 	import { base } from '$app/paths';
 
 	const title = `
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/super-central-browser' : ''
 		}
